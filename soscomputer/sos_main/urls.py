0f21b19c22729272dc4c385dd9c55c81ad1385d6
Sergey <44847286+WSERW@users.noreply.github.com>
2020-11-07 10:57:14 +0300

    seo optimization 2

--- soscomputer/sos_main/urls.py
+++ soscomputer/sos_main/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('robots.txt', TemplateView.as_view(template_name='sos_main/robots.txt', content_type='text/plain')),
     path('', views.index, name='index'),
     path('course', views.course, name='course'),
     path('services', views.services, name='services'),
