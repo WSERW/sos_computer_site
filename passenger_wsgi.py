95fb2f6d9db3ba68a3ef6b02c5d212208fb09f7f
Sergey <44847286+WSERW@users.noreply.github.com>
2021-05-16 09:24:35 +0300

    directory fix

--- passenger_wsgi.py
+++
@@ -1,8 +0,0 @@
-        
-# -*- coding: utf-8 -*-
-import os, sys
-sys.path.insert(0, '/var/www/u1130036/data/www/sos-computer.site/soscomputer')
-sys.path.insert(1, '/var/www/u1130036/data/djangoenv/lib/python3.9/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'soscomputer.settings'
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
